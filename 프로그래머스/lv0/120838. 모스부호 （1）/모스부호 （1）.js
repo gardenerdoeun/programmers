a2f573a094812a04d528d04294e2f0080afc9e91
gardenerdoeun <108854186+gardenerdoeun@users.noreply.github.com>
2023-03-21 16:25:18 +0900

    [level 0] Title: 모스부호 (1), Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    var answer = '';
+    var morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    };
+    letter = letter.split(' ');
+    for(let i = 0; i < letter.length; i++){
+        answer += morse[letter[i]];
+            //Object.keys(letter[i])
+    }
+    return answer;
+}
