252e12479cd622c0061de9d3f90e8e95455c5923
gardenerdoeun <108854186+gardenerdoeun@users.noreply.github.com>
2023-03-21 10:12:23 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,15 +1,10 @@
 function solution(age) {
     var answer = '';
-    var RealAge = ['a','b','c','d','e','f','g','h','i','j'];
+    var realAge = ['a','b','c','d','e','f','g','h','i','j'];
     age = String(age);
     
     for(let i = 0; i<age.length; i++){
-        
-        for(let j = 0; j < RealAge.length; j++){
-            if(Number(age[i]) === j){
-                answer+= RealAge[j];
-            }
-        }
+        answer+= realAge[age[i]];
     }
     return answer;
 }
