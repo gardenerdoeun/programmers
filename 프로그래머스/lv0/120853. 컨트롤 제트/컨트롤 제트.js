50eea65b2074b4413958438eef31ec01e1f9d5e7
gardenerdoeun <108854186+gardenerdoeun@users.noreply.github.com>
2023-04-05 20:37:05 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.19 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    var answer = 0;
+    s = s.split(' ');
+    for(let i = 0; i<s.length; i++){
+        if(s[i] === 'Z'){
+            answer -= Number(s[i-1]);
+        }else{
+            answer += Number(s[i]);
+        }
+    }
+    return answer;
+}
