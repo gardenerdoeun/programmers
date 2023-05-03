ccd590e41616adcb82e578c2bf22d7ea4a206820
gardenerdoeun <108854186+gardenerdoeun@users.noreply.github.com>
2023-05-04 00:00:43 +0900

    [level 0] Title: 외계어 사전, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -5,13 +5,13 @@ function solution(spell, dic) {
     for(let i=0; i<dic.length; i++){
 
         for(let j=0; j<spell.length; j++){
-            if(dic[i].indexOf(spell[j]) == -1){
+            if(dic[i].indexOf(spell[j]) === -1){
                 tmp++;
             } 
         }
         answer = tmp > 0 ? 2 : 1; 
         tmp = 0;
-        if(answer == 1) break;
+        if(answer === 1) break;
     }
 
     return answer;
